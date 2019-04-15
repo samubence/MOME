8b99c58573c5c81a6e20bef69ee17d955c37a04f
bence samu <samubence@gmail.com>
2019-04-15 17:34:04 +0200

    Lesson2

---
+++ __code__/Lesson2/Key.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public Animator animator;
+    public string animToPlay;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("COLLIDED");
+        Destroy(gameObject);
+        animator.Play(animToPlay);
+    }
+}
---
+++ __code__/Lesson2/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 up = new Vector3(0, 500, 0);
+    public Vector3 left = new Vector3(-50, 0, 0);
+    public Vector3 right = new Vector3(50, 0, 0);
+    public Vector3 forward = new Vector3(0, 0, 50);
+    public Vector3 backward = new Vector3(0, 0, -50);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(left);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(right);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(forward);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(backward);
+        }
+    }
+
+}
