ee3709b3a10eb831069b5c788ad1c06dea35d555
bence samu <samubence@gmail.com>
2019-04-15 15:35:50 +0200

    lesson 1 codes added

---
+++ __code__/Lesson1/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+
+        Destroy(gameObject);
+    }
+}
---
+++ __code__/Lesson1/Follower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public GameObject objToFollow;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - objToFollow.transform.position;    
+    }
+
+
+    void Update()
+    {
+        transform.position = objToFollow.transform.position + offset;    
+    }
+}
