55221844b1f10f4bc3c8c3bfaa82fb710acc4d10
bence samu <samubence@gmail.com>
2019-04-17 16:36:17 +0200

    Lesson3 added

---
+++ __code__/Lesson3/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public GameObject UI_Start;
+    public GameObject UI_End;
+
+    void Start()
+    {
+        UI_Start.SetActive(true);
+        UI_End.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStart()
+    {
+        UI_Start.SetActive(false);
+        UI_End.SetActive(false);
+    }
+
+    public void OnGameOver()
+    {
+        UI_Start.SetActive(false);
+        UI_End.SetActive(true);
+    }
+
+    public void OnRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ __code__/Lesson3/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameManager mainScript;      
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mainScript.OnGameOver();
+    }
+}
